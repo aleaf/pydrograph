48ff2b5c9487e6fc8f0b37bda3463770f2eee18a
aleaf <andrew.t.leaf@gmail.com>
2015-07-14 15:51:23 -0500

    pre merge

--- nwis.py
+++ nwis.py
@@ -145,3 +145,68 @@ class NWIS:
         shpdf = df.copy()
         shpdf['geometry'] = [Point(r.dec_long_va, r.dec_lat_va) for i, r in shpdf.iterrows()]
         GISio.df2shp(shpdf, shpname, epsg=4269)
+
+'''
+field measurements url:
+
+url = "http://waterdata.usgs.gov/nwis/measurements? \
+nw_longitude_va=-91.497& \
+nw_latitude_va=46.748&
+se_longitude_va=-90.228&
+se_latitude_va=46.156&
+coordinate_format=decimal_degrees&
+group_key=NONE&
+format=sitefile_output&
+sitefile_output_format=rdb&
+column_name=agency_cd&
+column_name=site_no&
+column_name=station_nm&
+column_name=site_tp_cd&
+column_name=lat_va&
+column_name=long_va&
+column_name=dec_lat_va&
+column_name=dec_long_va&
+column_name=coord_meth_cd&
+column_name=coord_acy_cd&
+column_name=coord_datum_cd&
+column_name=dec_coord_datum_cd&
+column_name=district_cd&
+column_name=state_cd&
+column_name=county_cd&
+column_name=country_cd&
+column_name=land_net_ds&
+column_name=map_nm&
+column_name=map_scale_fc&
+column_name=alt_va&
+column_name=alt_meth_cd&
+column_name=alt_acy_va&
+column_name=alt_datum_cd&
+column_name=huc_cd&
+column_name=basin_cd&
+column_name=topo_cd&
+column_name=data_types_cd&
+column_name=instruments_cd&
+column_name=construction_dt&
+column_name=inventory_dt&
+column_name=drain_area_va&
+column_name=contrib_drain_area_va&
+column_name=tz_cd&
+column_name=local_time_fg&
+column_name=reliability_cd&
+column_name=gw_file_cd&
+column_name=nat_aqfr_cd&
+column_name=aqfr_cd&
+column_name=aqfr_type_cd&
+column_name=well_depth_va&
+column_name=hole_depth_va&
+column_name=depth_src_cd&
+column_name=project_no&
+column_name=rt_bol&
+column_name=peak_begin_date&
+column_name=peak_end_date&
+column_name=peak_count_nu&
+column_name=qw_begin_date&column_name=qw_end_date&column_name=qw_count_nu&column_name=gw_begin_date&column_name=gw_end_date&column_name=gw_count_nu&column_name=sv_begin_date&column_name=sv_end_date&column_name=sv_count_nu&set_logscale_y=1&channel_html_info=0&date_format=YYYY-MM-DD&channel_rdb_info=0&rdb_compression=file&list_of_search_criteria=lat_long_bounding_box"
+
+Daily values url
+http://waterdata.usgs.gov/nwis/dv?referred_module=sw&site_tp_cd=ST&nw_longitude_va=-91&nw_latitude_va=47&se_longitude_va=-90&se_latitude_va=46&coordinate_format=decimal_degrees&group_key=NONE&format=sitefile_output&sitefile_output_format=rdb&column_name=agency_cd&column_name=site_no&column_name=station_nm&range_selection=days&period=365&begin_date=2014-04-14&end_date=2015-04-13&date_format=YYYY-MM-DD&rdb_compression=file&list_of_search_criteria=lat_long_bounding_box
+'''
