58b9401df87d342fb82c5bd5b637b3a92264b507
aleaf-usgs <aleaf@usgs.gov>
2016-06-15 15:00:29 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+
+setup(name='nwis',
+     description='Python package for working with NWIS.',
+     author='aleaf',
+     url='https://github.com/aleaf/NWIS.git',
+     packages=['nwis'])
+
