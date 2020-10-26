a56c900d83027ade3de4a3508652fdd56b556a89
GS\lschachter <lschachter@usgs.gov>
2020-10-26 14:32:23 -0500

    feat(get_ivs): added functionality to pull instantaneous values from NWIS

--- pydrograph/tests/test_nwis.py
+++ pydrograph/tests/test_nwis.py
@@ -4,6 +4,7 @@ from shapely.geometry import shape, box
 import pytest
 from gisutils import project, df2shp
 from pydrograph import Nwis
+from pandas._testing import assert_frame_equal
 
 
 @pytest.fixture(scope='session')
@@ -15,8 +16,8 @@ def extent_poly():
 
 @pytest.fixture(scope='session')
 def nwis_instance(extent_poly):
-    nwis = Nwis(extent=extent_poly)
-    return nwis
+    nwis_instance = Nwis(extent=extent_poly)
+    return nwis_instance
 
 
 @pytest.fixture(scope='session')
@@ -28,3 +29,9 @@ def field_sites(nwis_instance):
 def test_compute_geometries(extent_poly, nwis_instance, field_sites):
     geoms = nwis_instance._compute_geometries(field_sites)
     assert all([g.within(extent_poly) for g in geoms])
+    
+def test_instantaneous_value(nwis_instance):
+    #make sure all inputs are available for this function
+    #check that it creates a dataframe (use assert and exists)
+    df = nwis_instance.get_iv_siteinfo(attributes = 'iv_attributes')
+    assert_frame_equal(df, df)
